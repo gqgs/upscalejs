c65edea84d47ec7e6d0cf34321454e4bf66cafff
Guilherme <gqgs@protonmail.com>
2022-04-04 04:52:46 -0300

    worker: create and export type for valid models

--- src/image/worker.ts
+++ src/image/worker.ts
@@ -1,10 +1,13 @@
+
+export type Model = "no-denoise" | "conservative" | "denoise1x" | "denoise2x" | "denoise3x"
+
 interface Options {
   // Max number of workers that will be created for multiple images.
   maxWorkers?: number
   // Max number of worker that will be created for each image.
   maxInternalWorkers?: number
   // The model that will be used for upscaling the image.
-  denoiseModel?: "no-denoise" | "conservative" | "denoise1x" | "denoise2x" | "denoise3x"
+  denoiseModel?: Model
   // Environment base url (i.e. root of public path).
   base?: string
 }
