6fce07fd31905794f6c35f60b582cc5be6ff2baf
Guilherme <gqgs@protonmail.com>
2022-04-05 01:42:57 -0300

    grid: improve resizing feedback

--- src/components/ResizeGrid.vue
+++ src/components/ResizeGrid.vue
@@ -35,18 +35,37 @@
       </label>
     </div>
   </div>
-
   <div class="columns">
     <div class="is-12 column">
       <img v-if="input" class="px-2" :src="input">
       <canvas class="px-2" ref="resultcanvas" />
     </div>
   </div>
-
+  <div v-if="upscaling || done" class="columns is-centered" :class="{'done': done}">
+    <div class="is-4 column">
+      <div :class="[done ? 'is-success':'is-info']" class="notification is-link is-size-3 has-text-weight-bold">
+        <span v-if="done">Done!</span>
+        <span v-else>Upscaling. Please wait...</span>
+      </div>
+    </div>
+  </div>
   <div class="columns">
      <div class="is-10 is-offset-1 column"><progress v-if="upscaling" class="progress" max="100"></progress></div>
   </div>
 </template>
+
+<style scoped>
+.done {
+  animation: done 3s forwards;
+  animation-iteration-count: 1;
+}
+
+@keyframes done {
+    from { opacity: 100%; }
+    50% { opacity: 100%; }
+    to { opacity: 0%; }
+}
+</style>
 
 
 <script lang="ts">
@@ -57,6 +76,7 @@ const models = ["no-denoise", "conservative", "denoise1x", "denoise2x", "denoise
 const model = "conservative"
 const upscaling = ref(false)
 const active = ref(false)
+const done = ref(false)
 const input = ref("")
 const resultcanvas = ref()
 
@@ -68,25 +88,36 @@ const handleChange = (event: Event) => {
   upscale((event.target as HTMLInputElement).files)
 };
 
-const upscale = async (files?: FileList | null | undefined) => {
+const createImagePreview = (bitmap: ImageBitmap) => {
+  const canvas = resultcanvas.value as HTMLCanvasElement
+  canvas.width = bitmap.width * 2
+  canvas.height = bitmap.height * 2
+  canvas.getContext("2d")?.drawImage(bitmap, 0, 0, canvas.width, canvas.height)
+}
+
+const upscale = async (files?: FileList | null) => {
   if (!files || files.length === 0) {
     return
   }
-  upscaling.value = true
-  const file = files[0]
-  const bitmap = await createImageBitmap(file)
-  input.value = URL.createObjectURL(file)
   const upscaler = new UpscaleWorker({
     base: import.meta.env.BASE_URL,
     denoiseModel: model,
   })
-  const result = await upscaler.upscale(bitmap)
-  const canvas = resultcanvas.value as HTMLCanvasElement
-  canvas.width = result.width
-  canvas.height = result.height
-  canvas.getContext("bitmaprenderer")?.transferFromImageBitmap(result)
-  upscaler.terminate()
-  upscaling.value = false
+  try {
+    upscaling.value = true
+    done.value = false
+    const file = files[0]
+    const bitmap = await createImageBitmap(file)
+    createImagePreview(bitmap)
+    input.value = URL.createObjectURL(file)
+    const result = await upscaler.upscale(bitmap)
+    const canvas = resultcanvas.value as HTMLCanvasElement
+    canvas.getContext("2d")?.drawImage(result, 0, 0, result.width, result.height)
+  } finally {
+    upscaler.terminate()
+    upscaling.value = false
+    done.value = true
+  }
 }
 
 export default defineComponent({
@@ -100,6 +131,7 @@ export default defineComponent({
       model,
       active,
       handleDrop,
+      done,
     }
   }
 })
