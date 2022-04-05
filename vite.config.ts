858d77bed2fa728eb7636e71dd7e43627fa2ba9d
Guilherme <gqgs@protonmail.com>
2022-04-05 03:39:00 -0300

    build: register ion components

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,20 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+const vueOptions = {
+  template: {
+    compilerOptions: {
+      isCustomElement: (tag: string) => tag.startsWith("ion-")
+    }
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV == "production"
     ? "/upscalejs/"
     : "/",
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(vueOptions), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
